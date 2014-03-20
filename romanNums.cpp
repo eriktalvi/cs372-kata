0de02f806c7789481c1e1a33236f0a7b129a2554
Erik Talvi <eriktalvi@gmail.com>
2014-03-20 12:52:48 -0800

    created test_case and cpp

---
+++ romanNums.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN // This teels Catch to provide a main() - only do this in one cpp file
+#include <iostream>
+#include "catch.hpp"
+#include <string>
+#include <cmath>
+#include <vector>
+
+TEST_CASE( "Testing Romans", "[intToRoman] ) 
+{
+
+}
