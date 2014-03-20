1c65159cc1f636e6ed82e9a0b8f0ebf1c65b1708
Erik Talvi <eriktalvi@gmail.com>
2014-03-20 13:44:56 -0800

    18 to Rom for free

--- romanNums.cpp
+++ romanNums.cpp
@@ -44,4 +44,5 @@ TEST_CASE( "Testing Romans", "[intToRoman]" )
 	REQUIRE( intToRoman(15) == "XV" );
 	REQUIRE( intToRoman(16) == "XVI" );
 	REQUIRE( intToRoman(17) == "XVII" );
+	REQUIRE( intToRoman(18) == "XVIII" );
 }
